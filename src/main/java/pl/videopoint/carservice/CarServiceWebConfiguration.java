eedcba7aaf30fa911322595f777f756eb1e2c1e2
Lukasz <Lukasz@192.168.1.5>
2017-07-02 19:51:37 +0200

    Add web configuration

---
+++ src/main/java/pl/videopoint/carservice/CarServiceWebConfiguration.java
@@ -0,0 +1,10 @@
+package pl.videopoint.carservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.videopoint.carservice")
+public class CarServiceWebConfiguration {
+
+}
